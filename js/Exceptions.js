25d4e884af834e71624c4cc1a31ad4c1b1fbc1b3
Jose Miguel Muñoz <josemiguelmunoz013@gmail.com>
2023-01-13 17:21:31 +0100

    Create BaseException

---
+++ js/Exceptions.js
@@ -0,0 +1,10 @@
+"use strict";
+class BaseException extends Error {
+    constructor(message = "", fileName, lineNumber) {
+        super(message, fileName, lineNumber);
+        this.name = "BaseException";
+        if (Error.captureStackTrace) {
+            Error.captureStackTrace(this, BaseException);
+        }
+    }
+}
