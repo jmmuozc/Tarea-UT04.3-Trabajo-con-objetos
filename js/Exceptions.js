3c56ce707faa55d27841bf451995a7d20ceca805
jmmuozc <josemiguelmunoz013@gmail.com>
2023-01-14 02:08:03 +0100

    basic Exceptions

--- js/Exceptions.js
+++ js/Exceptions.js
@@ -8,3 +8,36 @@ class BaseException extends Error {
         }
     }
 }
+
+//Excepción acceso inválido a constructor
+class InvalidAccessConstructorException extends BaseException {
+    constructor(fileName, lineNumber) {
+        super("Constructor can’t be called as a Function.", fileName, lineNumber);
+        this.name = "Invalid Access Constructor Exception";
+    }
+}
+
+//Excepción String no cumple su patrón
+class InvalidString extends BaseException {
+    constructor(fileName, lineNumber) {
+        super("String is not valid according to its pattern.", fileName, lineNumber);
+        this.name = "Invalid String Exception";
+    }
+}
+
+//Excepción fecha es invalida
+class InvalidDate extends BaseException {
+    constructor(fileName, lineNumber) {
+        super("Date is not valid", fileName, lineNumber);
+        this.name = "Invalid Date Exception";
+    }
+}
+
+//Excepción número es invalida
+class InvalidNumber extends BaseException {
+    constructor(fileName, lineNumber) {
+        super("Number is not valid", fileName, lineNumber);
+        this.name = "Invalid Number Exception";
+    }
+}
+
