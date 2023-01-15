0b8b72e61e652c96334b04506ed2f8e2ec8e8c03
jmmuozc <josemiguelmunoz013@gmail.com>
2023-01-15 21:36:11 +0100

    test

--- js/test.js
+++ js/test.js
@@ -2,6 +2,7 @@ import Person from './Person.js';
 try {
     let person= new Person("Lucia","Ruiz","Sanchez","15,9,2002","example.jpg");
     console.log(person.getBorn());
+    console.log("test");
 } catch (error) {
     console.log(error.message);
 }
