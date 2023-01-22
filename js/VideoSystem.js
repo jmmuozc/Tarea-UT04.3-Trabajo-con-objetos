982214f00e1165ebf57b45a4be62cff59ede4286
jmmuozc <josemiguelmunoz013@gmail.com>
2023-01-22 16:59:32 +0100

    methods finished

--- js/VideoSystem.js
+++ js/VideoSystem.js
@@ -5,8 +5,10 @@ import Resource from "./Resource.js";
 import Movie from "./Movie.js";
 import Serie from "./Serie.js";
 import { stringPattern } from "./Modules.js";
-import { InvalidObject, InvalidString, CategoryExists, CategoryNoExists, UsernameExists, EmailExists, UserNoExists, UserExists,PersonExists,
-    ProductionExists,ProductionNoExists,ActorExists,ActorNoExists,DirectorExists,DirectorNoExists } from "./Exceptions.js";
+import {
+    InvalidObject, InvalidString, CategoryExists, CategoryNoExists, UsernameExists, EmailExists, UserNoExists, UserExists, PersonExists,
+    ProductionExists, ProductionNoExists, ActorExists, ActorNoExists, DirectorExists, DirectorNoExists
+} from "./Exceptions.js";
 import Production from "./Production.js";
 let videoSystem = (function () {
 
@@ -101,7 +103,7 @@ let videoSystem = (function () {
                 }
 
                 function compareElements(element) {
-                    return (element.dni === actor.dni)
+                    return (element.actor.dni === actor.dni)
                 }
 
                 return this.#ActorList.findIndex(compareElements);
@@ -113,7 +115,7 @@ let videoSystem = (function () {
                 }
 
                 function compareElements(element) {
-                    return (element.dni === director.dni)
+                    return (element.director.dni === director.dni)
                 }
 
                 return this.#DirectorList.findIndex(compareElements);
@@ -241,25 +243,25 @@ let videoSystem = (function () {
                 }
             }
 
-            addProductions(production) { 
+            addProductions(production) {
                 if (!(production instanceof Production)) throw new InvalidObject();
-                let position= this.#getProductionPosition(production);
-                if (position===-1) {
+                let position = this.#getProductionPosition(production);
+                if (position === -1) {
                     this.#ProductionsList.push(production);
-                }else throw new ProductionExists();
-                
+                } else throw new ProductionExists();
+
                 return this.#ProductionsList.length;
             }
-            
-            removeProductions(production){
+
+            removeProductions(production) {
                 if (!(production instanceof Production)) throw new InvalidObject();
-                if (!(this.#getProductionPosition(production)===-1)) throw new ProductionNoExists();
+                if (!(this.#getProductionPosition(production) === -1)) throw new ProductionNoExists();
                 let productionPosition;
                 for (let index of this.#ProductionsList) {
                     // Comprueba que la produccion exista dentro de cada array de producciones
-                    productionPosition=this.#getProductionPosition(production, this.#CategoriesList[index].productions);
-                    if ( productionPosition> -1) {
-                        this.#CategoriesList[index].productions.splice(productionPosition,1);
+                    productionPosition = this.#getProductionPosition(production, this.#CategoriesList[index].productions);
+                    if (productionPosition > -1) {
+                        this.#CategoriesList[index].productions.splice(productionPosition, 1);
                     }
                 }
                 return this.#ProductionsList.length;
@@ -278,19 +280,24 @@ let videoSystem = (function () {
                 }
             }
 
-            addActor(actor){
-                if (!(actor instanceof Production)) throw new InvalidObject();
-                position=this.#getActorPosition(actor);
-                if (position===-1) {
-                    this.#ActorList.push(actor);
-                }else throw new ActorExists();
+            addActor(actor) {
+                if (!(actor instanceof Person)) throw new InvalidObject();
+                position = this.#getActorPosition(actor);
+                if (position === -1) {
+                    this.#ActorList.push(
+                        {
+                            actor: actor,
+                            productions: []
+                        }
+                    );
+                } else throw new ActorExists();
 
                 return this.#ActorList.length;
             }
 
             removeActor(actor) {
                 if (!(actor instanceof Person)) throw new InvalidObject();
-                let position=this.#getActorPosition(actor);
+                let position = this.#getActorPosition(actor);
                 if (position != -1) {
                     this.#ActorList.splice(position, 1);
                 } else throw new ActorNoExists();
@@ -310,38 +317,174 @@ let videoSystem = (function () {
                 }
             }
 
-            addDirector(director){
-                if (!(director instanceof Production)) throw new InvalidObject();
-                position=this.#getDirectorPosition(director);
-                if (position===-1) {
-                    this.#ActorList.push(director);
-                }else throw new DirectorExists();
+            addDirector(director) {
+                if (!(director instanceof Person)) throw new InvalidObject();
+                position = this.#getDirectorPosition(director);
+                if (position === -1) {
+                    this.#DirectorList.push(
+                        {
+                            director: director,
+                            productions: []
+                        }
+                    );
+                } else throw new DirectorExists();
 
-                return this.#ActorList.length;
+                return this.#DirectorList.length;
             }
 
             removeDirector(director) {
                 if (!(director instanceof Person)) throw new InvalidObject();
-                let position=this.#getDirectorPosition(director);
+                let position = this.#getDirectorPosition(director);
                 if (position != -1) {
                     this.#DirectorList.splice(position, 1);
                 } else throw new DirectorNoExists();
                 return this.#DirectorList.length;
             }
 
-            assignCategory(category,production){
-
+            assignCategory(category, ...production) {
+                if (!(category instanceof Category)) throw new InvalidObject();
+                let catPosition = this.#getCategoryPosition(category);
+                if (catPosition === -1) this.addCategory(category);
+                production.forEach(element => {
+                    if (!(element instanceof Production)) throw new InvalidObject();
+                    if (this.#getProductionPosition(element) === -1) this.addProductions(element);
+                    if (this.#getProductionPosition(element, this.#CategoriesList[catPosition].productions) === -1) {
+                        this.#CategoriesList[catPosition].productions.push(element);
+                    }
+                });
+                return this.#CategoriesList[catPosition].length;
             }
 
-            deassignCategory(category,production){
+            deassignCategory(category, ...production) {
+                if (!(category instanceof Category)) throw new InvalidObject();
+                let catPosition = this.#getCategoryPosition(category);
+                if (catPosition === -1) throw new CategoryNoExists;
+                let prodPosition;
+                production.forEach(element => {
+                    if (!(element instanceof Production)) throw new InvalidObject();
+                    if (this.#getProductionPosition(element) > -1) {
+                        prodPosition = this.#getProductionPosition(element, this.#CategoriesList[catPosition].productions);
+                        if (prodPosition > -1) {
+                            this.#CategoriesList[catPosition].productions.splice(prodPosition, 1);
+                        }
+                    }else console.log("Production does not exists");
+                });
+                return this.#CategoriesList[catPosition].length;
+            }
 
+            assignDirector(director, ...production){
+                if (!(director instanceof Person)) throw new InvalidObject();
+                let directorPosition = this.#getDirectorPosition(director);
+                if (directorPosition === -1) this.#DirectorList.push(director);
+                production.forEach(element => {
+                    if (!(element instanceof Production)) throw new InvalidObject();
+                    if (this.#getProductionPosition(element) === -1) this.addProductions(element);
+                    if (this.#getProductionPosition(element, this.#DirectorList[directorPosition].productions) === -1) {
+                        this.#DirectorList[directorPosition].productions.push(element);
+                    }
+                });
+                return this.#DirectorList[directorPosition].length;
+            }
+
+            deassignDirector(director, ...production){
+                if (!(director instanceof Person)) throw new InvalidObject();
+                let directorPosition = this.#getCategoryPosition(director);
+                if (directorPosition === -1) throw new DirectorNoExists;
+                let prodPosition;
+                production.forEach(element => {
+                    if (!(element instanceof Production)) throw new InvalidObject();
+                    if (this.#getProductionPosition(element) > -1) {
+                        prodPosition = this.#getProductionPosition(element, this.#DirectorList[directorPosition].productions);
+                        if (prodPosition > -1) {
+                            this.#DirectorList[directorPosition].productions.splice(prodPosition, 1);
+                        }
+                    }else console.log("Production does not exists");
+                });
+                return this.#DirectorList[directorPosition].length;
+            }
+
+            assignActor(actor, ...production){
+                if (!(actor instanceof Person)) throw new InvalidObject();
+                let actorPosition = this.#getActorPosition(director);
+                if (actorPosition === -1) this.#ActorList.push(director);
+                production.forEach(element => {
+                    if (!(element instanceof Production)) throw new InvalidObject();
+                    if (this.#getProductionPosition(element) === -1) this.addProductions(element);
+                    if (this.#getProductionPosition(element, this.#ActorList[actorPosition].productions) === -1) {
+                        this.#ActorList[actorPosition].productions.push(element);
+                    }
+                });
+                return this.#ActorList[actorPosition].length;
+            }
+
+            deassignActor(actor, ...production){
+                if (!(actor instanceof Person)) throw new InvalidObject();
+                let actorPosition = this.#getActorPosition(actor);
+                if (actorPosition === -1) throw new ActorNoExists();
+                let prodPosition;
+                production.forEach(element => {
+                    if (!(element instanceof Production)) throw new InvalidObject();
+                    if (this.#getProductionPosition(element) > -1) {
+                        prodPosition = this.#getProductionPosition(element, this.#ActorList[actorPosition].productions);
+                        if (prodPosition > -1) {
+                            this.#ActorList[actorPosition].productions.splice(prodPosition, 1);
+                        }
+                    }else console.log("Production does not exists");
+                });
+                return this.#ActorList[actorPosition].length;
+            }
+
+            *getCast(production){
+                if (!(production instanceof Production)) throw new InvalidObject();
+                let CastArray=[];
+                let prodPosition;
+                this.#ActorList.forEach(element =>{
+                    prodPosition=this.#getProductionPosition(production,element.productions);
+                    if(prodPosition>-1){
+                        CastArray.push(element.productions[prodPosition]);
+                    }
+                });
+
+                for (let cast of CastArray){
+                    yield cast;
+                }
+            }
+
+            *getProductionsDirector(director){
+                if (!(director instanceof Person)) throw new InvalidObject();
+
+                let arrayProductionsDirector= this.#DirectorList[this.#getDirectorPosition(director)].productions;
+
+                for (let productions of arrayProductionsDirector){
+                    yield productions;
+                }
+            }
+
+            *getProductionsActor(actor){
+                if (!(actor instanceof Person)) throw new InvalidObject();
+
+                let arrayProductionsActor= this.#ActorList[this.#getActorPosition(actor)].productions;
+
+                for (let productions of arrayProductionsActor){
+                    yield productions;
+                }
+            }
+
+            *getProductionsCategory(category){
+                if (!(category instanceof Category)) throw new InvalidObject();
+
+                let arrayProductionsCategory= this.#ProductionsList[this.#getCategoryPosition(category)].productions;
+
+                for (let productions of arrayProductionsCategory){
+                    yield productions;
+                }
             }
 
             personFactory(name, dni, lastname1, born, lastname2 = "Example", picture = "Base.jpg") {
                 let createdPerson = new Person(name, dni, lastname1, born, lastname2, picture);
                 let positionActor = this.#getActorPosition(createdPerson);
                 let positionDirector = this.#getDirectorPosition(createdPerson);
-                if (positionDirector == -1 && positionActor ==-1) {
+                if (positionDirector == -1 && positionActor == -1) {
                     return createdPerson;
                 } else throw new PersonExists();
 
@@ -357,20 +500,20 @@ let videoSystem = (function () {
 
             }
 
-            movieFactory(title, publication, nationality = "NaN", synopsis = "", image = "default.png", resource = new Resource(5, "example.png")){
-                let createdMovie = new Movie(title, publication, nationality,synopsis,image,resource);
-                let position=this.#getProductionPosition(createdMovie);
-                if (position===-1) {
+            movieFactory(title, publication, nationality = "NaN", synopsis = "", image = "default.png", resource = new Resource(5, "example.png")) {
+                let createdMovie = new Movie(title, publication, nationality, synopsis, image, resource);
+                let position = this.#getProductionPosition(createdMovie);
+                if (position === -1) {
                     return createdMovie;
-                }else throw new ProductionExists();
+                } else throw new ProductionExists();
             }
 
-            serieFactory(title, publication, nationality = "NaN", synopsis = "", image = "default.png", resource = new Resource(5, "example.png")){
-                let createdSerie = new Serie(title, publication, nationality,synopsis,image,resource);
-                let position=this.#getProductionPosition(createdSerie);
-                if (position===-1) {
+            serieFactory(title, publication, nationality = "NaN", synopsis = "", image = "default.png", resource = new Resource(5, "example.png")) {
+                let createdSerie = new Serie(title, publication, nationality, synopsis, image, resource);
+                let position = this.#getProductionPosition(createdSerie);
+                if (position === -1) {
                     return createdSerie;
-                }else throw new ProductionExists();
+                } else throw new ProductionExists();
             }
         }
 
